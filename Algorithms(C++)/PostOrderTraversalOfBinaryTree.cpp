6c3a11499a5fdb9daf53ef4e175963afa840b000
KumkumMittal <72305495+KumkumMittal@users.noreply.github.com>
2022-10-13 22:31:10 +0530

    Added tree from inorder and preorder

---
+++ Algorithms(C++)/PostOrderTraversalOfBinaryTree.cpp
@@ -0,0 +1,46 @@
+//Given a Binary Tree, find out the PostOrder Traversal.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+//Approach 1
+//Recursive code
+struct node {
+  int data;
+  struct node * left, * right;
+};
+
+void postOrderTrav(node * curr, vector < int > & postOrder) {
+  if (curr == NULL)
+    return;
+
+  postOrderTrav(curr -> left, postOrder);
+  postOrderTrav(curr -> right, postOrder);
+  postOrder.push_back(curr -> data);
+}
+
+//Approach 2
+//Iterative code
+vector < int > postOrderTrav(node * curr) {
+
+  vector < int > postOrder;
+  if (curr == NULL) return postOrder;
+
+  stack < node * > s1;
+  stack < node * > s2;
+  s1.push(curr);
+  while (!s1.empty()) {
+    curr = s1.top();
+    s1.pop();
+    s2.push(curr);
+    if (curr -> left != NULL)
+      s1.push(curr -> left);
+    if (curr -> right != NULL)
+      s1.push(curr -> right);
+  }
+  while (!s2.empty()) {
+    postOrder.push_back(s2.top() -> data);
+    s2.pop();
+  }
+  return postOrder;
+}
