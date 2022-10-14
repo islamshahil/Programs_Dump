372df244e7027b6a7a3944c22b2482249c1a7b6a
KumkumMittal <72305495+KumkumMittal@users.noreply.github.com>
2022-10-14 23:00:09 +0530

    Next permutation

---
+++ Algorithms(C++)/Next Permutation.cpp
@@ -0,0 +1,28 @@
+// Given an array of integers nums, find the next permutation of nums.
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+     int n=nums.size(), i, j;
+        for(i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1])
+                break;
+        }
+        if(i<0){
+        reverse(nums.begin(), nums.end());
+            return;
+        }
+        else{
+            for(j=n-1;j>i;j--){
+                if(nums[j]>nums[i])
+                    break;
+            }
+        }
+        swap(nums[i],nums[j]);
+        reverse(nums.begin()+i+1, nums.end());
+    }
+};
